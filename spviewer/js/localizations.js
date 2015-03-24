1c1f3e6acd3c42a99a775a04213a373f5dee4bd6
Yoshi Sakai <yoshi.sakai@gmail.com>
2015-03-24 14:05:01 +0900

    add l10n.js and twitter bootstrap

---
+++ spviewer/js/localizations.js
@@ -0,0 +1,22 @@
+String.toLocaleString({
+	"en": {
+		"%title": "SPViewer(en)",
+		"%info": "Radioisotope spectrum viewer",
+                "%menu1": "Select",
+                "%menu2": "View",
+                "%menu3": "Setting",
+		"%spectrumList": "Select Spectrum",
+		"%spectrumView": "View Spectrum",
+		"%spectrumEdit": "Spectrum Setting"
+	},
+	"ja": {
+		"%title": "SPViewer(ja)",
+                "%info": "放射線スペクトル表示ツール",
+                "%menu1": "スペクトル選択",
+                "%menu2": "スペクトル表示",
+                "%menu3": "描画要素変更",
+		"%spectrumList": "スペクトル選択",
+		"%spectrumView": "スペクトル表示",
+		"%spectrumEdit": "セッティング変更"
+	}
+});
